291f0fb8a2348f5f09c4d83d3cc6dc839789fad4
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-24 11:51:37 +0100

    Add mongoose connection.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,19 @@
+import mongoose from "mongoose";
+
 import app from "./app";
 
 const setup = async () => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_KEY env variable should be definde.");
+  }
+
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
   app.listen("5000", () =>
     console.log("Auth server is listening on port 5000")
   );
