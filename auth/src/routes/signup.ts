f31ffce2013786fc54c5263a2cf7fa29af494215
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-24 14:42:52 +0100

    Create SignUp router.

---
+++ auth/src/routes/signup.ts
@@ -0,0 +1,54 @@
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { StatusCodes } from "http-status-codes";
+
+import { validateRequest } from "../middlewares";
+import { BadRequestError } from "../errors";
+import { User } from "../models";
+
+const router = express.Router();
+
+const passwodLength = { min: 8, max: 100 };
+const userNameLength = { min: 2, max: 100 };
+
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength(passwodLength)
+      .withMessage(
+        `Password must be between ${passwodLength.min} and ${passwodLength.max} characters`
+      ),
+    body("userName")
+      .trim()
+      .isLength(userNameLength)
+      .withMessage(
+        `User name must be between ${userNameLength.min} and ${userNameLength.max} characters`
+      ),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password, userName, location, address } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email is already in use.");
+    }
+
+    const newUser = User.build({
+      email,
+      password,
+      userName,
+      location,
+      address,
+    });
+    await newUser.save();
+
+    res.status(StatusCodes.CREATED).send(newUser);
+  }
+);
+
+export { router as signupRouter };
