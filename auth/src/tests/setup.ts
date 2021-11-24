05fc64a6b56bc66a6d8608c0f18d920cb009c2b7
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-24 16:10:00 +0100

    Add global signin() function to Jest setup file.

--- auth/src/tests/setup.ts
+++ auth/src/tests/setup.ts
@@ -1,7 +1,14 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request, { Response } from "supertest";
+
+import app from "../app";
 
 let mongo: MongoMemoryServer;
+
+declare global {
+  function signin(): Promise<[Response, string[]]>;
+}
 
 beforeAll(async () => {
   process.env.JWT_KEY = "somekey";
@@ -25,3 +32,28 @@ afterAll(async () => {
   await mongoose.connection.close();
   await mongo.stop();
 });
+
+global.signin = async () => {
+  const user = {
+    email: "test@test.com",
+    password: "password",
+    userName: "user",
+    location: {
+      lat: 53.86182,
+      long: 10.66172,
+      radius: 1500,
+    },
+    address: {
+      city: "Kyiv",
+      street: "Hlybochytska St, 32А",
+      country: "Ukraine",
+      countryCode: "02000",
+    },
+  };
+
+  const response = await request(app).post("/api/users/signup").send(user);
+
+  const cookie = response.get("Set-Cookie");
+
+  return [response, cookie];
+};
