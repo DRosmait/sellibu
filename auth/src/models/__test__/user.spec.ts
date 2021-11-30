9222d1e3dfa67870366e8e649543efadeacf314b
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-11-30 14:43:24 +0100

    Extend user model tests in Auth.

--- auth/src/models/__test__/user.spec.ts
+++ auth/src/models/__test__/user.spec.ts
@@ -13,4 +13,52 @@ describe("User model", () => {
 
     expect(userFromDB!.id).toBeDefined();
   });
+
+  it("implements optimistic concurrency control", async () => {
+    const user = User.build({
+      email: "test@test.com",
+      password: "password",
+      userName: "Max Mustermann",
+    });
+    await user.save();
+
+    const firstInstance = await User.findById(user.id);
+    const secondInstance = await User.findById(user.id);
+
+    const firstSave = async () => {
+      firstInstance!.set({ password: "first change" });
+      await firstInstance!.save();
+    };
+
+    const secondSave = async () => {
+      secondInstance!.set({ password: "first change" });
+      await secondInstance!.save();
+    };
+
+    await firstSave();
+    expect(firstInstance!.version).toEqual(1);
+
+    await expect(secondSave).rejects.toThrow();
+    expect(secondInstance!.version).toEqual(0);
+  });
+
+  it("increments version number on each save.", async () => {
+    const user = User.build({
+      email: "test@test.com",
+      password: "password",
+      userName: "Max Mustermann",
+    });
+
+    await user.save();
+    expect(user.version).toEqual(0);
+
+    await user.save();
+    expect(user.version).toEqual(1);
+
+    await user.save();
+    expect(user.version).toEqual(2);
+
+    await user.save();
+    expect(user.version).toEqual(3);
+  });
 });
