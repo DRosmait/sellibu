5c2147db593d683ed33bf23f89b2b13851c3379f
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-12-01 12:41:04 +0100

    Add createAds() helper function for Ads unit tests.

--- ads/src/routes/__tests__/helpers/index.ts
+++ ads/src/routes/__tests__/helpers/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
+import { AdStatus } from "@sellibu-proj/common";
 
-import { User } from "../../../models";
+import { Ad, User, UserDoc } from "../../../models";
 
 export async function addUserToDB() {
   const user = User.build({
@@ -11,4 +12,37 @@ export async function addUserToDB() {
   await user.save();
 
   return user;
+}
+
+export function createAds({
+  count = 10,
+  closedCount = 2,
+  userDoc,
+}: {
+  count: number;
+  closedCount: number;
+  userDoc?: UserDoc;
+}) {
+  if (count < closedCount)
+    throw new Error(
+      "createAds() function expect 'count' greater than 'closedCount'"
+    );
+
+  return Promise.race(
+    Array(count)
+      .fill("")
+      .map(async (_, idx) => {
+        const user = userDoc || (await addUserToDB());
+        const isClosed = idx < count - closedCount - 1;
+        const ad = Ad.build({
+          title: `Ad #${idx}`,
+          description: `Description for Ad #${idx}`,
+          price: Math.floor(Math.random() * 100) + 1,
+          user,
+          status: isClosed ? AdStatus.Open : AdStatus.Closed,
+        });
+
+        return ad;
+      })
+  );
 }
