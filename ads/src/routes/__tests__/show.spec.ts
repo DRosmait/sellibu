0f52a215bb6b1298cae14a7fbdd2a731aa07f736
Dima Rosmait <57414478+DRosmait@users.noreply.github.com>
2021-12-01 13:43:37 +0100

    Create showAdRouter in Ads. (#8)
    Co-authored-by: Dima Rosmait <dima.rosmait@soilandmore.com>

---
+++ ads/src/routes/__tests__/show.spec.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { StatusCodes } from "http-status-codes";
+
+import app from "../../app";
+import { createAds } from "./helpers";
+
+describe("show.ts", () => {
+  it(`returns ${StatusCodes.NOT_FOUND} if ad not found.`, async () => {
+    await request(app)
+      .get("/api/ads/" + new mongoose.Types.ObjectId().toHexString())
+      .send()
+      .expect(StatusCodes.NOT_FOUND);
+  });
+
+  it(`returns ${StatusCodes.OK} if ad found.`, async () => {
+    const ads = await createAds();
+    const ad = ads[5];
+
+    const { body } = await request(app)
+      .get("/api/ads/" + ads[5].id)
+      .send()
+      .expect(StatusCodes.OK);
+
+    expect(body.title).toEqual(ad.title);
+    expect(body.description).toEqual(ad.description);
+    expect(body.price).toEqual(ad.price);
+    expect(body.user.id).toEqual(ad.user.id);
+  });
+});
