43d27610086babbc0f4b2f6c6364cfc2053d5fd2
Dima Rosmait <57414478+DRosmait@users.noreply.github.com>
2021-12-02 19:36:49 +0100

    Ads infra (#12)
    * Ad Ads to Skaffold and Ingress.
    
    * Remove yarn.lock file from .dockerignore.
    
    * Fix bugs in index.ts in Ads.
    
    Co-authored-by: Dima Rosmait <dima.rosmait@soilandmore.com>

--- ads/src/index.ts
+++ ads/src/index.ts
@@ -3,15 +3,15 @@ import mongoose from "mongoose";
 import app from "./app";
 
 const setup = async () => {
-  if (process.env.JWT_KEY) {
-    throw new Error("JWT_KEY evn variable must bu define.");
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY env variable must be definde.");
   }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect("mongodb://ads-mongo-srv:27017/ads");
     console.log("Connected to MongoDB");
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 
   app.listen(5000, () => console.log("Ads server is listening on port 5000"));
