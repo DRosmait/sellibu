b9d2ebca4bccd11533026b3d7e3ee79392e6f732
Dima Rosmait <57414478+DRosmait@users.noreply.github.com>
2021-12-03 15:56:14 +0100

    Nats listener (#15)
    * Create Subjects enum in shared lib.
    
    * Create abstract Listener class enum in shared lib.
    
    Co-authored-by: Dima Rosmait <dima.rosmait@soilandmore.com>

---
+++ shared/src/events/subjects.ts
@@ -0,0 +1,12 @@
+export enum Subjects {
+  UserCreated = "user:created",
+  UserUpdated = "user:updated",
+
+  UserContactDataUpdated = "user:contact:data:updated",
+
+  AdCreated = "ad:created",
+  AdOpen = "ad:open",
+  AdHoldOn = "ad:hold:on",
+  AdClosed = "ad:closed",
+  AdExpired = "ad:expired",
+}
