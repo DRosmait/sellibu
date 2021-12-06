dc073a26240dba7aff80c9bb05d00cbbf091e2b6
Dima Rosmait <57414478+DRosmait@users.noreply.github.com>
2021-12-06 17:40:30 +0100

    Ads expiration date (#18)
    * Ad "createdAt" and "expiresAt" fields to Ads model.
    
    * Fix unit tests.
    
    Co-authored-by: Dima Rosmait <dima.rosmait@soilandmore.com>

--- ads/src/models/ad.ts
+++ ads/src/models/ad.ts
@@ -21,11 +21,18 @@ export interface AdDoc extends mongoose.Document {
   user: UserDoc;
   userId: string;
   status: AdStatus;
+  createdAt: string;
+  expiresAt: string;
   version: number;
 }
 
 interface AdModel extends mongoose.Model<AdDoc> {
   build(attrs: AdAttrs): AdDoc;
+}
+
+function getMonthsInFuture(monthCount: number, date = new Date()) {
+  date.setMonth(date.getMonth() + monthCount);
+  return date.toISOString();
 }
 
 const adSchema = new mongoose.Schema(
@@ -42,20 +49,27 @@ const adSchema = new mongoose.Schema(
       required: true,
       type: Number,
     },
-    status: {
-      required: true,
-      type: String,
-      enum: Object.values(AdStatus),
-      default: AdStatus.Open,
-    },
     user: {
       required: true,
       type: mongoose.Types.ObjectId,
       ref: "User",
     },
     userId: {
+      type: String,
+    },
+    status: {
       required: true,
       type: String,
+      enum: Object.values(AdStatus),
+      default: AdStatus.Open,
+    },
+    createdAt: {
+      type: String,
+      default: new Date().toISOString(),
+    },
+    expiresAt: {
+      type: String,
+      default: new Date().toISOString(),
     },
   },
   {
@@ -71,12 +85,14 @@ const adSchema = new mongoose.Schema(
 adSchema.set("versionKey", "version");
 adSchema.plugin(updateIfCurrentPlugin);
 
-adSchema.statics.build = (attrs: AdAttrs) => {
-  return new Ad({
-    ...attrs,
-    userId: attrs.user.id,
-  });
-};
+adSchema.pre("save", function (done) {
+  this.set("userId", this.user.id);
+  this.set("expiresAt", getMonthsInFuture(1, new Date(this.createdAt)));
+
+  done();
+});
+
+adSchema.statics.build = (attrs: AdAttrs) => new Ad(attrs);
 
 const Ad = mongoose.model<AdDoc, AdModel>("Ad", adSchema);
 
