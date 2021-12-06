1194525b1530395ab993ff20b6f4829748e46ade
Dima Rosmait <57414478+DRosmait@users.noreply.github.com>
2021-12-06 18:27:42 +0100

    Talks - initial setup (#19)
    * Add boiler plate for Talks service.
    
    * Remove test workflow for Talks.
    
    Co-authored-by: Dima Rosmait <dima.rosmait@soilandmore.com>

---
+++ talks/src/index.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+import app from "./app";
+import natsWrapper from "./nats-wrapper";
+
+const setup = async () => {
+  // Mandatory ENV variables
+  const variableNames = [
+    "NATS_CLIENT_ID",
+    "NATS_URL",
+    "NATS_CLUSTER_ID",
+    "MONGO_URI",
+    "JWT_KEY",
+  ];
+
+  variableNames.forEach((varName) => {
+    if (!process.env[varName]) {
+      throw new Error(`${varName} env variable must be definde.`);
+    }
+  });
+
+  try {
+    // Connect to NATS-Streaming
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    // Listen to NATS messages
+
+    // Connect to Mongoose DB
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(5000, () => console.log("Ads server is listening on port 5000"));
+};
+
+setup();
---
+++ talks/src/tests/setup.ts
@@ -0,0 +1,51 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+jest.mock("../nats-wrapper");
+
+let mongo: MongoMemoryServer;
+
+declare global {
+  function signin(userId?: string): string[];
+}
+
+beforeAll(async () => {
+  process.env.JWT_KEY = "somekey";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+
+  const collections = mongoose.connection.collections;
+
+  await Promise.all(
+    Object.values(collections).map((collection) => collection.deleteMany({}))
+  );
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongo.stop();
+});
+
+global.signin = (userId?: string) => {
+  const payload = {
+    id: userId || new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+    userName: "Max Mustermann",
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`express:sess=${base64}`];
+};
