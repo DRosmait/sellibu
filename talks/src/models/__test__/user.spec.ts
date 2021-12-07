e28944f61cc533f11b5131a5ac5dd8325d87cba8
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-12-07 17:29:21 +0100

    Create User model in Talks.

---
+++ talks/src/models/__test__/user.spec.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+import { User } from "..";
+
+describe("User model", () => {
+  it("has 'id' field.", async () => {
+    const user = User.build({
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: "test@test.com",
+      userName: "Max Mustermann",
+    });
+    await user.save();
+
+    const userFromDB = await User.findById(user.id);
+
+    expect(userFromDB!.id).toBeDefined();
+  });
+
+  it("implements optimistic concurrency control", async () => {
+    const user = User.build({
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: "test@test.com",
+      userName: "Max Mustermann",
+    });
+    await user.save();
+
+    const firstInstance = await User.findById(user.id);
+    const secondInstance = await User.findById(user.id);
+
+    const firstSave = async () => {
+      firstInstance!.set({ password: "first change" });
+      await firstInstance!.save();
+    };
+
+    const secondSave = async () => {
+      secondInstance!.set({ password: "second change" });
+      await secondInstance!.save();
+    };
+
+    await firstSave();
+    expect(firstInstance!.version).toEqual(1);
+
+    await expect(secondSave).rejects.toThrow();
+    expect(secondInstance!.version).toEqual(0);
+  });
+
+  it("increments version number on each save.", async () => {
+    const user = User.build({
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: "test@test.com",
+      userName: "Max Mustermann",
+    });
+
+    await user.save();
+    expect(user.version).toEqual(0);
+
+    await user.save();
+    expect(user.version).toEqual(1);
+
+    await user.save();
+    expect(user.version).toEqual(2);
+
+    await user.save();
+    expect(user.version).toEqual(3);
+  });
+});
