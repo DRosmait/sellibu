e6a181f040cfcc750f021c122dc23291f7742adf
Dima Rosmait <dima.rosmait@soilandmore.com>
2021-12-10 17:49:30 +0100

    Create helpers for tests in Talks.

---
+++ talks/src/routes/__tests__/helpers/index.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { AdStatus } from "@sellibu-proj/common";
+import { Ad, User } from "../../../models";
+
+export function getRandomMongooseId() {
+  return new mongoose.Types.ObjectId().toHexString();
+}
+
+export const createAdOwnerUser = async () => {
+  const owner = User.build({
+    id: getRandomMongooseId(),
+    email: "max@test.com",
+    userName: "Max Mustermann",
+  });
+  await owner.save();
+
+  const user = User.build({
+    id: getRandomMongooseId(),
+    email: "lisa@test.com",
+    userName: "Lisa Musterfrau",
+  });
+  await user.save();
+
+  const ad = Ad.build({
+    title: "Some ad",
+    price: 100,
+    user: owner,
+    status: AdStatus.Open,
+  });
+  await ad.save();
+
+  return { owner, user, ad };
+};
